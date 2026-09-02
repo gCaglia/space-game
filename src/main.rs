0d70d7fc25155ee3ad71a0748b3482a9f1a20914
Giulia Caglia <cagliagiulia42@gmail.com>
2026-09-02 18:47:55 +0200

    Add background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("SpaceGame")]
+async fn main() {
+    let bytes = include_bytes!("../assets/space.png");
+    let background: Texture2D = Texture2D::from_file_with_format(bytes, Some(ImageFormat::Png));
+
+    loop {
+        clear_background(WHITE);
+        draw_texture_ex(
+            &background,
+            0.0,
+            0.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(screen_width(), screen_height())),
+                ..Default::default()
+            },
+        );
+        next_frame().await
+    }
 }
